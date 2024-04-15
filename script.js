9d85c05c71562bf851091fdbfc946d8d97ebd150
Pedro <pedrowpvh@hotmail.com>
2024-04-15 13:30:01 -0400

    hot fix icon

--- script.js
+++ script.js
@@ -3,12 +3,10 @@ let chave = "cebcd482eda57fa9a6714c1c2ba91885"
 async function buscarCidade(cidade){
     let dados = await fetch( "https://api.openweathermap.org/data/2.5/weather?q="+cidade+"&appid=cebcd482eda57fa9a6714c1c2ba91885&units=metric")
     .then(resposta => resposta.json());
-    console.log(dados);
     exibirNaTela(dados)
 }
 function cliqueNoBotao() {
     let cidade = document.querySelector(".input").value;
-    console.log(cidade)
     buscarCidade(cidade);
 }
 
@@ -17,9 +15,7 @@ function exibirNaTela (dados) {
     document.querySelector(".cidade").innerHTML = "Tempo em " + dados.name;
     document.querySelector(".grau").innerHTML = Math.floor(dados.main.temp) + "°C";
     document.querySelector(".umidade").innerHTML = "Umidade: " + dados.main.humidity + "%";
-    document.querySelector(".icon").src = "https://openweathermap.org/img/wn/"+dados.weather[0].icon+".png";
+    document.querySelector(".icon").src = "https://openweathermap.org/img/wn/"+dados.weather[0].icon+".png";    
     }
 
-
-
-
+    
