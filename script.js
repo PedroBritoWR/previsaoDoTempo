46c8f4aed3798f9e4871d077b7b8253b50605687
Pedro <pedrowpvh@hotmail.com>
2024-04-15 14:00:16 -0400

    Tentando fazer um evento pra capturar a tecla enter

--- script.js
+++ script.js
@@ -1,21 +1,35 @@
 let chave = "cebcd482eda57fa9a6714c1c2ba91885"
 
-async function buscarCidade(cidade){
-    let dados = await fetch( "https://api.openweathermap.org/data/2.5/weather?q="+cidade+"&appid=cebcd482eda57fa9a6714c1c2ba91885&units=metric")
-    .then(resposta => resposta.json());
-    exibirNaTela(dados)
+async function buscarCidade(cidade) {
+    let dados = await fetch("https://api.openweathermap.org/data/2.5/weather?q=" + cidade + "&appid=cebcd482eda57fa9a6714c1c2ba91885&units=metric")
+        .then(resposta => resposta.json());
+    exibirNaTela(dados);
+
 }
 function cliqueNoBotao() {
     let cidade = document.querySelector(".input").value;
     buscarCidade(cidade);
+    limparInput();
 }
 
 
-function exibirNaTela (dados) {
+function exibirNaTela(dados) {
     document.querySelector(".cidade").innerHTML = "Tempo em " + dados.name;
     document.querySelector(".grau").innerHTML = Math.floor(dados.main.temp) + "°C";
     document.querySelector(".umidade").innerHTML = "Umidade: " + dados.main.humidity + "%";
-    document.querySelector(".icon").src = "https://openweathermap.org/img/wn/"+dados.weather[0].icon+".png";    
-    }
+    document.querySelector(".icon").src = "https://openweathermap.org/img/wn/" + dados.weather[0].icon + ".png";
+}
 
-    
+function limparInput() {
+    document.querySelector(".input").value = '';
+}
+
+
+const inputElement = document.querySelector('.input');
+
+inputElement.addEventListener('keypress', (event) => {
+    if (event.key === 'Enter') {
+        event.preventDefault();
+        cliqueNoBotao();
+    }
+});
